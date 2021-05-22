6af427c04e01015d843a2581e2c24d964992168d
Almat Kulbaev <almat.kulbaev@yandex.ru>
2021-05-22 13:52:45 +0700

    init

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    title = "Contacts"
+    view.backgroundColor = .systemBlue
+  }
+
+}
+
